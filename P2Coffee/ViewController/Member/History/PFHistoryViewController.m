0d765b368be2e6ab080824ec9c7a5d999a4e66ec
promjai <pariwat@Pariwats-Mac.local>
2014-06-17 10:38:22 +0700

    up
    up

---
+++ P2Coffee/ViewController/Member/History/PFHistoryViewController.m
@@ -0,0 +1,38 @@
+//
+//  PFHistoryViewController.m
+//  P2Coffee
+//
+//  Created by Pariwat on 6/17/14.
+//  Copyright (c) 2014 Platwo fusion. All rights reserved.
+//
+
+#import "PFHistoryViewController.h"
+
+@interface PFHistoryViewController ()
+
+@end
+
+@implementation PFHistoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
