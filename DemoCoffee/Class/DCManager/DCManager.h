cb8b85ef8f99dcc55f4e81abd5eca72022051328
aommiez <aommiez@gmail.com>
2014-06-20 12:26:41 +0700

    demo init project & config

---
+++ DemoCoffee/Class/DCManager/DCManager.h
@@ -0,0 +1,13 @@
+//
+//  DCManager.h
+//  DemoCoffee
+//
+//  Created by MRG on 6/20/2557 BE.
+//  Copyright (c) 2557 Platwo fusion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCManager : NSObject
+
+@end
---
+++ DemoCoffee/Class/DCManager/DCManager.m
@@ -0,0 +1,13 @@
+//
+//  DCManager.m
+//  DemoCoffee
+//
+//  Created by MRG on 6/20/2557 BE.
+//  Copyright (c) 2557 Platwo fusion. All rights reserved.
+//
+
+#import "DCManager.h"
+
+@implementation DCManager
+
+@end
