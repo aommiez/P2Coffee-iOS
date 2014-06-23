bc3f8fa9075f216cf8b7ef38ccd57fae8c21d7ae
Pariwat <wat_promjai@hotmail.com>
2014-06-23 16:46:02 +0700

    up
    up

---
+++ DemoCoffee/ViewController/Menu/Order/PFOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  PFOrderViewController.h
+//  DemoCoffee
+//
+//  Created by Pariwat on 6/23/14.
+//  Copyright (c) 2014 Platwo fusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFOrderViewController : UIViewController
+
+@end
---
+++ DemoCoffee/ViewController/Menu/Order/PFOrderViewController.m
@@ -0,0 +1,38 @@
+//
+//  PFOrderViewController.m
+//  DemoCoffee
+//
+//  Created by Pariwat on 6/23/14.
+//  Copyright (c) 2014 Platwo fusion. All rights reserved.
+//
+
+#import "PFOrderViewController.h"
+
+@interface PFOrderViewController ()
+
+@end
+
+@implementation PFOrderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
